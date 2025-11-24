5c1bf63c5db2b2587f8e58762374395ad51141e4
Brahim Chater <m0217681@student.vives.be>
2025-11-24 21:55:10 +0100

    File to initialize SQLite database with tables 'patienten' and 'taken'

---
+++ app/db.py
@@ -0,0 +1,59 @@
+import sqlite3
+import os
+
+
+# Assigns the desired database folder, file and full path to a variable
+db_path = "data"
+db_file = "tasks.db"
+db_full_path = os.path.join(db_path, db_file)
+
+
+# Defines a function with which we can initialize the database
+def initialize_db():
+    # If the directory "data" doesn't exist it should first be created
+    if not os.path.exists(db_path):
+        os.makedirs(db_path, exist_ok=True)
+    
+    #Create a database if it doesn't exist yet and connect it. 
+    #Then create a cursor object
+    with sqlite3.connect(db_full_path) as db:
+        my_cursor = db.cursor()
+    
+        # Query to create a table "patienten" in the database
+        query_pt = """
+        CREATE TABLE IF NOT EXISTS patienten (
+            id,
+            naam,
+            geboorte_datum,
+            telefoon,
+            opmerkingen
+            )
+        """
+        
+        # Create the table "patienten"
+        my_cursor.execute(query_pt)
+    
+        # Query to create a table "taken" in the database
+        query_taken = """
+        CREATE TABLE IF NOT EXISTS taken (
+            id,
+            patient_id,
+            omschrijving,
+            datum_aanmaak,
+            prioriteit,
+            deadline,
+            status,
+            voltooid_op,
+            opmerkingen_afhandeling
+            )
+        """
+        
+        # Create the table "taken"
+        my_cursor.execute(query_taken)
+        # Commit the changes to the database     
+        db.commit()
+        
+
+if __name__ == "__main__":
+    initialize_db()
+    print(f"Het databasebestand {db_file} en tabellen zijn aangemaakt in map: {db_path} ")

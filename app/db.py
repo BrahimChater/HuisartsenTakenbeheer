8c45108949c8af1b2d926dbc1ede00510a22667f
Brahim Chater <m0217681@student.vives.be>
2025-11-28 12:44:52 +0100

    Adjusting the update_patient function in db.py to be able to  update only selected fields. Adding test code

--- app/db.py
+++ app/db.py
@@ -4,7 +4,12 @@ from models import Patient, Taak
 
 # Assigns the desired database folder, file and full path to a variable
 db_path = "data"
-db_file = "tasks.db"
+db_file = "task_manager.db"
+
+#Testdb
+#db_path ="test/data"
+#db_file = "test_tasks.db"
+
 db_full_path = os.path.join(db_path, db_file)
 
 
@@ -86,45 +91,90 @@ def add_patient(naam, geboorte_datum, telefoon, opmerkingen="/"):
 
         # If the patient doesn't exist then a new entry is created and its id returned
         else:
-             query = """
+            query = """
              INSERT INTO patienten (naam, geboorte_datum, telefoon, opmerkingen)
              VALUES (?,?,?,?)
              """
 
-             input_values = (naam, geboorte_datum, telefoon, opmerkingen)
+            input_values = (naam, geboorte_datum, telefoon, opmerkingen)
 
-             my_cursor.execute(query, input_values)
-             db.commit()
+            my_cursor.execute(query, input_values)
+            db.commit()
 
-             # With INTEGER PRIMARY KEY for id an integer id was automatically created by SQLite
-             # Lastrowid returns the id
-             return my_cursor.lastrowid
-         
-            
-# Defines a function to update a patient
-def update_patient(id, naam, geboorte_datum, telefoon, opmerkingen="/"):
-    
+            # With INTEGER PRIMARY KEY for id an integer id was automatically created by SQLite
+            # Lastrowid returns the id
+            return my_cursor.lastrowid
+
+
+# Defines a function to update a patient. Only the selected fields will be updated
+def update_patient(id, naam=None, geboorte_datum=None, telefoon=None, opmerkingen=None):
+
     # Makes sure the database and tables exist
     initialize_db()
-    
-    #Update the table patienten for a given id
+
+    # Update the table patienten for a given id
     with sqlite3.connect(db_full_path) as db:
         my_cursor = db.cursor()
-        
-        query="""
+
+        # 1. Select the row from the database you want to update
+        query_select = """
+        SELECT naam, geboorte_datum, telefoon, opmerkingen
+        FROM patienten
+        WHERE id= ?
+        """
+
+        input_value = (id,)
+
+        my_cursor.execute(query_select, input_value)
+
+        selected_row = my_cursor.fetchone()
+
+        if selected_row is None:
+            print("Geen patient gevonden met dit id!")
+            return None
+
+        # 2. Fill in the new updated values
+        current_naam = selected_row[0]
+        current_geboorte_datum = selected_row[1]
+        current_telefoon = selected_row[2]
+        current_opmerkingen = selected_row[3]
+
+        if naam is None:
+            new_naam = current_naam
+        else:
+            new_naam = naam
+
+        if geboorte_datum is None:
+            new_geboorte_datum = current_geboorte_datum
+        else:
+            new_geboorte_datum = geboorte_datum
+
+        if telefoon is None:
+            new_telefoon = current_telefoon
+        else:
+            new_telefoon = telefoon
+
+        if opmerkingen is None:
+            new_opmerkingen = current_opmerkingen
+        else:
+            new_opmerkingen = opmerkingen
+
+        # 3. Update the database entry
+        query_update = """
         UPDATE patienten
         SET naam = ?,
             geboorte_datum = ?,
-            telefoon = ?,
+            telefoon =?,
             opmerkingen =?
         WHERE id = ?
         """
-        
-        input_values = (naam, geboorte_datum, telefoon, opmerkingen, id)
-        
-        my_cursor.execute(query,input_values)
+
+        update_values = (new_naam, new_geboorte_datum,
+                         new_telefoon, new_opmerkingen, id)
+
+        my_cursor.execute(query_update, update_values)
+
         db.commit()
-        
 
 
 # Defines a function to get all patient entries from the database and return them as a list of patient objects
@@ -200,16 +250,18 @@ def add_task(patient_id, omschrijving, datum_aanmaak, deadline, prioriteit="norm
             return my_cursor.lastrowid
 
 # Defines a function to update a task
+
+
 def update_task(id, patient_id, omschrijving, datum_aanmaak, deadline, prioriteit="normaal", status="lopende", voltooid_op="niet voltooid", opmerkingen_afhandeling="/"):
-    
+
     # Makes sure the database and tables exist
     initialize_db()
-    
-    #Update the table taken for a given id
+
+    # Update the table taken for a given id
     with sqlite3.connect(db_full_path) as db:
         my_cursor = db.cursor()
-        
-        query="""
+
+        query = """
         UPDATE taken
         SET patient_id = ?,
             omschrijving = ?,
@@ -221,7 +273,7 @@ def update_task(id, patient_id, omschrijving, datum_aanmaak, deadline, prioritei
             opmerkingen_afhandeling = ?
         WHERE id = ?
         """
-        
+
         input_values = (
             patient_id,
             omschrijving,
@@ -233,10 +285,9 @@ def update_task(id, patient_id, omschrijving, datum_aanmaak, deadline, prioritei
             opmerkingen_afhandeling,
             id,
         )
-        
-        my_cursor.execute(query,input_values)
-        db.commit()
 
+        my_cursor.execute(query, input_values)
+        db.commit()
 
 
 # Defines a function to get all tasks for a patient from the database and return them as a list of task objects
@@ -256,7 +307,6 @@ def get_all_tasks_for_patient(patient_id):
         WHERE patient_id = ?
         """
 
-
         my_cursor.execute(query, (patient_id,))
         rows = my_cursor.fetchall()
 
@@ -273,15 +323,15 @@ def get_all_tasks_for_patient(patient_id):
             opmerkingen_afhandeling = row[8]
 
             taak = Taak(
-               id,
-               patient_id,
-               omschrijving,
-               datum_aanmaak,
-               deadline,
-               prioriteit,
-               status,
-               voltooid_op,
-               opmerkingen_afhandeling,
+                id,
+                patient_id,
+                omschrijving,
+                datum_aanmaak,
+                deadline,
+                prioriteit,
+                status,
+                voltooid_op,
+                opmerkingen_afhandeling,
             )
             tasks.append(taak)
 
@@ -290,38 +340,70 @@ def get_all_tasks_for_patient(patient_id):
 
 # Testing the code
 if __name__ == "__main__":
+    
     initialize_db()
     print(
         f"Het databasebestand {db_file} en tabellen zijn aangemaakt in map: {db_path} ")
 
     # Test: Add a patient
-    a = add_patient("Patient Zero", "1950-01-01", "0123456789", "Test Patient")
+    a = add_patient("Patient Zero", "1950-01-01", "0123456789", "mut in orde")
+    b = add_patient("Patient One", "1970-10-01", "987654321", "ocmw patient")
+    c = add_patient("Patient Two", "2010-12-01","22244442222555", "fedasil patient")
 
-    # Test: Add a task for that patient
-    b = add_task(
-       patient_id=a,
-       omschrijving="Verwijsbrief cardiologie maken",
-       datum_aanmaak="2025-11-22",
-       deadline="2025-11-30",
-       prioriteit="hoog",
+    # Test: Add a task for some patients
+    d = add_task(
+        patient_id=a,
+        omschrijving="Verwijsbrief cardiologie maken",
+        datum_aanmaak="2025-11-22",
+        deadline="2025-11-30",
+        prioriteit="hoog",
     )
-    print(f"Taak {b} toegevoegd voor patiënt {a}")
-   
+    print(f"Taak {d} toegevoegd voor patiënt {a}\n")
 
-    
-    #Test: Get and print all patient entries
-    
+    e = add_task(
+        patient_id=b,
+        omschrijving="Document FOD Handicap invullen",
+        datum_aanmaak="2025-11-27",
+        deadline="2025-12-15",
+        prioriteit="normaal",
+    )
+    print(f"Taak {e} toegevoegd voor patiënt {b}\n")
+
+    f = add_task(
+        patient_id=c,
+        omschrijving="Afspraak dermatologie maken",
+        datum_aanmaak="2025-11-28",
+        deadline="2025-12-30",
+        prioriteit="laag",
+    )
+    print(f"Taak {f} toegevoegd voor patiënt {c}\n")
+
+    # Test: Get and print all patient entries
+
     patients = get_all_patients()
     for p in patients:
         print(p)
-    
+
     # Test: Get and print all tasks for this patient
+    print("Alle taken voor patiënt a")
     tasks = get_all_tasks_for_patient(a)
     for t in tasks:
         print(t)
 
-    
-    
-    
-    
-    
+    print("Alle taken voor patiënt b")
+    tasks = get_all_tasks_for_patient(b)
+    for t in tasks:
+        print(t)
+
+    print("Alle taken voor patiënt c")
+    tasks = get_all_tasks_for_patient(c)
+    for t in tasks:
+        print(t)
+
+    # Test: update telefoon and opmerkingen for patient a and check change in patient entries
+    print(f"Alle patienten met update van gegevens voor patiënt met id {a} (voor de velden telefoon, opmerkingen)")
+    update_patient(id=a, telefoon="098666664321", opmerkingen="Geen geldige mutualiteit meer")
+    patients = get_all_patients()
+    for p in patients:
+        print(p)
+

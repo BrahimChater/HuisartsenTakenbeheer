d0b0d212a3feea64ea6e4a4dd2523cece8ab85e2
Brahim Chater <m0217681@student.vives.be>
2025-11-29 11:15:37 +0100

    Adding an example settings file

---
+++ example_settings.py
@@ -0,0 +1,10 @@
+# Example configuration for the tool Takenmanager Huisartsen
+
+# Folder where the SQLite database will be stored
+db_set_dir = "data"
+
+# SQLite database filename
+db_set_file = "task_manager.db"
+
+# Default folder for csv exports
+export_set_dir = "export"
